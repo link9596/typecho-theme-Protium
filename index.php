642f72124545f2e0e142b7cc47366b7bcc2bf855
Link <1316341442@qq.com>
2021-11-03 19:45:48 +0800

    1.3.0

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * 
  * @package Protium for Typecho
  * @author Link
- * @version 1.2.0
+ * @version 1.3.0
  * @link https://atlinker.cn
  */
 
@@ -12,6 +12,13 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
  $this->need('header.php');
  $this->need('/include/sticky.php');
  ?>
+
+<?php if (!empty($this->options->search) && in_array('search', $this->options->search)): ?>
+<div style="margin:-28px 0px 20px 0px" class="form-group">
+                 <form id="search" role="search" method="post" action="<?php $this->options->siteUrl(); ?>" >
+                 <input type="text" name="s" class="form-control" id="s" required="required" onkeydown=KeyDown()/>
+                 <label for="s" class="form-label">Search</label>
+               </div><?php endif; ?>
 
 	<?php while($this->next()): ?>
          <ul style="" class="post-list">
@@ -33,15 +40,15 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 <div style="margin-bottom:-35px" class="pagination">
 
       <?php if ($this->_currentPage>1){ ?>
-					<?php $this->pageLink('<span class="cm-l"><img src="<?php $this->options->themeUrl('/assets/icon/left.svg'); ?>" style="height:28px;margin-left:12px"></span>'); ?>
+					<?php $this->pageLink('<span class="cm-l"><img src="/usr/themes/Protium/assets/icon/left.svg" style="height:28px;margin-left:12px"></span>'); ?>
 				<?php } else { ?>
-					<span class="cm-l"><img src="<?php $this->options->themeUrl('/assets/icon/leftdis.svg'); ?>" style="height:28px;margin-left:12px"></span>
+					<span class="cm-l"><img src="/usr/themes/Protium/assets/icon/leftdis.svg" style="height:28px;margin-left:12px"></span>
 				<?php } ?>
 				<?php $totalpage=ceil($this->getTotal()/$this->parameter->pageSize); ?>
 				<?php if ($this->_currentPage<$totalpage){ ?>
-					<?php $this->pageLink('<span class="cm-r"><img src="<?php $this->options->themeUrl('/assets/icon/right.svg'); ?>" style="height:28px;margin-right:14px"></span>','next'); ?>
+					<?php $this->pageLink('<span class="cm-r"><img src="/usr/themes/Protium/assets/icon/right.svg" style="height:28px;margin-right:14px"></span>','next'); ?>
 				<?php } else { ?>
-					<span class="cm-r"><img src="<?php $this->options->themeUrl('/assets/icon/rightdis.svg'); ?>" style="height:28px;margin-right:14px"></span>
+					<span class="cm-r"><img src="/usr/themes/Protium/assets/icon/rightdis.svg" style="height:28px;margin-right:14px"></span>
 				<?php } ?>
 			<div class="cm-c"><?php if($this->_currentPage>1) echo $this->_currentPage; else echo 1;?>/<?php echo ceil($this->getTotal() / $this->parameter->pageSize); ?></div>
 </div>
