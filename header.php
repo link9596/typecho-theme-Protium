a70b67ab1ab5746edfe37f18c87490767379c74d
Link <1316341442@qq.com>
2021-11-03 20:23:58 +0800

    1.3.0

--- header.php
+++ header.php
@@ -75,7 +75,7 @@
 <?php else: ?>
 <?php $this->archiveTitle(array(
             'category'  =>  _t('分类 %s 下的文章'),
-            'search'    =>  _t('包含关键字 %s 的文章'),
+            'search'    =>  _t('按关键字搜索'),
             'tag'       =>  _t('标签 %s 下的文章'),
             'author'    =>  _t('%s 发布的文章')
         ), '', '  '); ?>
